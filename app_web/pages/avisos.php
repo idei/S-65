1a4a588ca45d46158ef5c31d14def8fd8fce66d8
fabriciocarrio <fabriciocarrio@gmail.com>
2022-12-15 14:37:22 -0300

    template

--- app_web/pages/avisos.php
+++ app_web/pages/avisos.php
@@ -21,7 +21,7 @@
                     <!-- Stack the columns on mobile by making one full-width and the other half-width -->
                     <div class="row" style="margin: 100; color: rgb(92, 90, 90);">
                       <div class="p-5">
-                        <h2>Ver datos de paciente</h2>
+                        <h2>Avisos</h2>
                         <div class="row">
                             <div class="p-2">
                             <input type="search" class="form-control rounded" placeholder="Escribir DNI de paciente" aria-label="Search" aria-describedby="search-addon" />
@@ -33,30 +33,6 @@
                       </div>
                     </div>
                 </div>  
-  <!-- Modal -->
-  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">Datos de paciente</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          Nombre: Manuel
-        </br>
-          Apellido: Olivares
-        </br>
-          Contacto: Sergio Albarado
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-          <a href="datos.html" class="btn btn-primary active" role="button" aria-pressed="true">Aceptar</a>
-        </div>
-      </div>
-    </div>
-  </div>
         </div>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
---
+++ app_web/pages/navbar_template.php
@@ -0,0 +1,28 @@
+<!-- Sidebar-->
+<div class="border-end bg-white" id="sidebar-wrapper">
+                <div class="sidebar-heading border-bottom bg-light"><img class="img-fluid" width="150" heigth="100"src="../assets/logo1.png" alt="Chania" height="100%"></div>
+                <div class="list-group list-group-flush">
+                    <div><i class="fa-solid fa-skating fa-fw"></i><a class="list-group-item list-group-item-action list-group-item-light p-3" href="inicio.php">Pacientes</a></div>
+                    
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="avisos.php">Avisos</a>
+                </div>
+            </div>
+            <!-- Page content wrapper-->
+            <div id="page-content-wrapper">
+                <!-- Top navigation-->
+                <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                    <div class="container-fluid">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span></button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">User</a>
+                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="#!">Cerrar Sesión</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
