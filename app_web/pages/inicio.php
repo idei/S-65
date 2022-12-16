43f744eb1878c7dfb92d2d5a721dc6e5013fb9b1
Carolina Olivares <38932158+CarolinaOli@users.noreply.github.com>
2022-12-16 01:39:05 -0300

    datos

--- app_web/pages/inicio.php
+++ app_web/pages/inicio.php
@@ -24,11 +24,13 @@
                         <h2>Ver datos de paciente</h2>
                         <div class="row">
                             <div class="p-2">
-                            <input type="search" class="form-control rounded" placeholder="Escribir DNI de paciente" aria-label="Search" aria-describedby="search-addon" />
+                            <form method="POST">
+                            <input id="dni" type="search" class="form-control rounded" placeholder="Escribir DNI de paciente" aria-label="Search" aria-describedby="search-addon" />
                             </div>
                             <div class="p-2">
-                                <button type="button" class="btn btn-primary p-2" data-toggle="modal" data-target="#exampleModalCenter">Buscar</button>
+                                <button onclick="buscar()" type="button" class="btn btn-primary p-2" data-toggle="modal" data-target="#exampleModalCenter">Buscar</button>
                             </div>
+                            </form>
                           </div>
                       </div>
                     </div>
@@ -52,13 +54,41 @@
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-          <a href="datos.html" class="btn btn-primary active" role="button" aria-pressed="true">Aceptar</a>
+          <a href="datos.php"   class="btn btn-primary active" role="button" aria-pressed="true">Aceptar</a>
         </div>
       </div>
     </div>
   </div>
         </div>
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script>
+	function buscar()
+    {
+
+      var parametros = 
+      {
+        "dni" : document.getElementById("dni").value,
+      };
+
+      $.ajax({
+        data: parametros,
+        url: '../php/user_read_datos_personales.php',
+        type: 'POST',
+        dataType:"JSON",
+    
+        success: function(response)
+        {        
+            if(response['request']=='Success'){   
+              console.log("paso")
+            }else{
+                console.log("nop");
+            }
+        }
+      });
+}
+
+
+</script>
+<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     </body>
